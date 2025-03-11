4a0250f27f1738663d3a0eae58d879ed1706ae79
Nika-D-Lin <yilin1.24@intl.zju.edu.cn>
2025-03-11 16:24:30 +0800

    Add files via upload

---
+++ Practical4/bmi_calculator.py
@@ -0,0 +1,13 @@
+'''
+You can input your own data to have a look at your BMI.
+And you will know whether you are considered underweight, normal or obese.
+'''
+weight = float(input("input your weight(kg):"))
+height = float(input("input your height(m):"))
+BMI = weight/(height)**2
+if BMI > 30:
+    print(f"Your BMI is {BMI}. You are obese.")
+elif BMI < 18.5:
+    print(f"Your BMI is {BMI}.You are underweight.")
+else:
+    print(f"Your BMI is {BMI}.You are normal.")
---
+++ Practical4/triangle.py
@@ -0,0 +1,6 @@
+'''
+We have learned that this kind of series have a formul as what listed below.
+'''
+for num in range(1,11):
+    nums = num * (num + 1)/2
+    print(nums)
