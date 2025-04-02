46b7d2e6844c8440b6512324ef98896874339694
Li Yilin <yilin1.24@intl.zju.edu.cn>
2025-04-02 16:17:53 +0800

    add commit

--- Practical7/spliced_tata_genes.py
+++ Practical7/spliced_tata_genes.py
@@ -1,11 +1,11 @@
 import re
-
+#when find the next name, append the last name and seqyence
 input0 = open(r"C:\Users\lenovo\Downloads\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r')
 out_put = []
-current_gene = None
+current_gene = None #at first, there is no name
 current_sequence = []
 for line in input0:
-    line = line.strip()
+    line = line.strip() #delete the blank character
     if re.search('>',line):  # New gene sequence
         if current_gene:
             out_put.append((current_gene[0], ''.join(current_sequence)))
@@ -19,8 +19,8 @@ if current_gene: #to check the final one
 
 spliced_gene = input("chooce one:GTAG, GCAG, ATAC ")
 input2 = open(fr'C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical7\{spliced_gene}_spliced_genes.fa','w')
-for gene_name, sequence in out_put:
-        if re.search(r'TATA[AT]A[AT]', sequence):
-            if re.search(fr'{spliced_gene}', sequence):
+for gene_name, sequence in out_put: #use two new varieties to meet the two kinds--current_gene and current_sequence
+        if re.search(r'TATA[AT]A[AT]', sequence): #use [] to search 'TATATAT' or 'TATATAA' or 'TATAAAT' or 'TATAAAA'
+            if re.search(fr'{spliced_gene}', sequence): #only print what users chooce
                 number = len(re.findall(r'TATA[AT]A[AT]', sequence))
                 input2.write(f'> {gene_name}\n{sequence}\n{number}\n')
--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,16 +1,16 @@
 import re
-
+#when find the next name, append the last name and seqyence
 input = open(r"C:\Users\lenovo\Downloads\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa",'r')
 out_put = []
-current_gene = None
+current_gene = None #at first, there is no name
 current_sequence = []
 for line in input:
-    line = line.strip()
+    line = line.strip() #delete the blank character
     if re.search('>',line):  # New gene sequence
         if current_gene:
-            out_put.append((current_gene[0], '\n'.join(current_sequence)))
-        current_gene = (re.findall(r'gene:(\S+)',line))
-        current_sequence = []
+            out_put.append((current_gene[0], '\n'.join(current_sequence))) #use \n to make each line in different lines
+        current_gene = (re.findall(r'gene:(\S+)',line)) #only use the name by ()
+        current_sequence = [] #clear the list for the next name to use
     else:
         current_sequence.append(line)
         
@@ -18,6 +18,6 @@ if current_gene: #to check the final one
     out_put.append((current_gene[0], '\n'.join(current_sequence)))
 
 input2 = open(r'C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical7\tata_genes.fa','w')
-for gene_name, sequence in out_put:
-        if re.search(r'TATA[AT]A[AT]', sequence):
+for gene_name, sequence in out_put: #use two new varieties to meet the two kinds--current_gene and current_sequence
+        if re.search(r'TATA[AT]A[AT]', sequence): #use [] to search 'TATATAT' or 'TATATAA' or 'TATAAAT' or 'TATAAAA'
             input2.write(f'> {gene_name}\n{sequence}\n')
