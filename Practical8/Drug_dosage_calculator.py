f4faa45ddd807200c596e5d71dc2024bd8f3db4d
Li Yilin <yilin1.24@intl.zju.edu.cn>
2025-04-09 15:13:47 +0800

    change the input style of strength

--- Practical8/Drug_dosage_calculator.py
+++ Practical8/Drug_dosage_calculator.py
@@ -1,17 +1,20 @@
 def Drug_dosage_calculator(weight, strength_of_paracetamol): #define a new function with these three parameters
-    volume = 15 * weight / strength_of_paracetamol #calculate the theoretical volume
     if weight < 10 or weight > 100:
         return('Error, the weight is not in the required range.')
     else:
-        if strength_of_paracetamol not in [24,50]: #compare the therotical value and input one
+        if strength_of_paracetamol not in ['120mg/5ml', '250mg/5ml']: #compare the therotical value and input one
             return(f"Error, the strength is not in the required range.")
         else:
+            if strength_of_paracetamol == '120mg/5ml':
+                volume = 15 * weight / 24 #calculate the theoretical volume
+            else:
+                volume = 15 * weight / 50 #calculate the theoretical volume
             return(f"Correct, the therotical value is {volume}.")
         
 weight = int(input("weight:"))        
-strength = int(input('chooce one from 120mg/5ml and 250mg/5ml (just input "24" or "50"):'))
-print (Drug_dosage_calculator(weight,strength))
+strength = input('chooce one from 120mg/5ml and 250mg/5ml :')
+print(Drug_dosage_calculator(weight,strength))
 
 #examaple:
-print(Drug_dosage_calculator(80,50))
+print(Drug_dosage_calculator(80,'250mg/5ml'))
 #it will print 'Correct, the therotical value is 24.'
