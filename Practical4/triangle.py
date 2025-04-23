3034bda25f15d90b218b07ebf402926559283789
Li Yilin <yilin1.24@intl.zju.edu.cn>
2025-04-23 15:29:18 +0800

    add detailed commit

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,6 +1,6 @@
 '''
 We have learned that this kind of series have a formul as what listed below.
 '''
-for num in range(1,11):
-    nums = num * (num + 1)/2
-    print(round(nums))
+for num in range(1,11): #use the circulation to print the total number for 10 times
+    nums = num * (num + 1)/2 #It is the formula we learned
+    print(round(nums)) #use round function to keep integer (the decimal part is zero, so use this function can just delete the decimal part)
