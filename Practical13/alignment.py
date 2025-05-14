eec67f5f21336cf7d8c44e1804abb3ddbca52b16
Li Yilin <yilin1.24@intl.zju.edu.cn>
2025-05-14 16:01:22 +0800

    simplify the code and add the interpretation text

--- Practical13/alignment.py
+++ Practical13/alignment.py
@@ -7,47 +7,39 @@ def comparasion(seq1, seq2):
             same_sequence += 1
     return f"{same_sequence/len(seq1)*100}%"
 
-with open(r"C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical13\human_sod2.fasta") as f1:
-    lines = f1.readlines()
-    sequence = []
-    for line in lines:
-        if not re.search(r">",line):
-            sequence.append(line)
-seq1 = ''.join(sequence)
+def sequence(seq):
+    with open(seq) as f1:
+        lines = f1.readlines()
+        sequence = []
+        for line in lines:
+            if not re.search(r">",line):
+                sequence.append(line.strip()) #use strip() to delete the space
+    return ''.join(sequence) #to put each line together in one line
 
-with open(r"C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical13\mouse_sod2.fasta") as f2:
-    lines = f2.readlines()
-    sequence = []
-    for line in lines:
-        if not re.search(r">",line):
-            sequence.append(line)
-seq2 = ''.join(sequence)
+seq1 = r"C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical13\human_sod2.fasta"
+seq2 = r"C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical13\mouse_sod2.fasta"
+seq3 = r"C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical13\random.fasta"
 
-with open(r"C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical13\random.fasta") as f3:
-    lines = f3.readlines()
-    sequence = []
-    for line in lines:
-        if not re.search(r">",line):
-            sequence.append(line)
-seq3 = ''.join(sequence)
+seq1 = sequence(seq1)
+seq2 = sequence(seq2)
+seq3 = sequence(seq3)
 
 with open(r"C:\Users\lenovo\OneDrive - International Campus, Zhejiang University\桌面\Github\IBI1_2024-25\Practical13\BLOSUM62.txt") as f4:
     lines = f4.readlines()
     sequence = []
     for line in lines:
         if not re.search(r'#',line):
-            sequence.append(line.strip())
+            sequence.append(line)
 matrix = sequence
 
 def BLOSUM62_calculate(seq1, seq2):
-    diffenrent = []
+    pairs = []
     for	i in range(len(seq1)):	
-        if seq1[i] != seq2[i]:
-            diffenrent.append(seq1+seq2)
+        pairs.append(seq1[i]+seq2[i])
 
     score = 0
-    for pair in diffenrent:
-        for i in range(len(matrix[0])):
+    for pair in pairs:
+        for i in range(len(matrix[0])): #use two for circle to read the matrix
             for j in range(len(matrix)):
                 if pair[0] == matrix[0][i] and pair[1] == matrix[j][0]:
                     score += int(matrix[j][i])
@@ -55,5 +47,5 @@ def BLOSUM62_calculate(seq1, seq2):
 
 group = [("human and mouse", seq1, seq2), ("human and random", seq1, seq3), ("mouse and random", seq2, seq3)]
 
-for label, seq1, seq2 in group:
+for label, seq1, seq2 in group: #use the three eletment groups to fit the three varieties
     print(f"As for {label}: the identity is {comparasion(seq1, seq2)}, and the BLOSUM62 is {BLOSUM62_calculate(seq1, seq2)}")
